ae2528867c19da3fe4faeed1c26563597a833aed
Mari <nctzenmaria@gmail.com>
2026-05-13 09:13:02 -0300

    Create dados.py

---
+++ dados.py
@@ -0,0 +1,20 @@
+# Lista principal
+filmes = []
+
+# Fila FIFO de reservas
+fila_reservas = []
+
+# Pilha LIFO de devoluções
+pilha_devolucoes = []
+
+# Tuplas fixas
+status_disponibilidade = ("Disponível", "Indisponível")
+
+generos = (
+    "Ação",
+    "Comédia",
+    "Drama",
+    "Terror",
+    "Ficção",
+    "Romance"
+)
